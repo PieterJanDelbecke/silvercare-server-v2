a9977cbbfece6e770d14d005f68b5ab2e67ed592
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-11-21 14:16:05 +1100

    fix: sequelize working with env file

---
+++ config/sequelizer.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config({ path: ".env.dev" });
+
+module.exports = {
+	database: process.env.DB_DATABASE,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	dialect: process.env.DB_DIALECT,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+};
