2ec6d2e744874fb337cd44c74656d7235ff04a52
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2024-01-22 10:59:21 +1100

    get organisedActivity routing

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const logger = require("../lib/logger");
 
 const { QueryTypes } = require("sequelize");
-const { OrganisedActivity, OrganisedActivityAttendence, sequelize } = require("../models");
+const { OrganisedActivity, OrganisedActivityAttendence, Resident, Activity, sequelize } = require("../models");
 
 router.post("/new", async (req, res) => {
 	console.log("activity/new: ", req.body);
@@ -29,6 +29,41 @@ router.post("/new", async (req, res) => {
 	} catch (error) {
 		logger.error("POST activity/new: ", error);
 		res.send("ERROR: POST activity/new");
+	}
+});
+
+router.get("/organisedActivity", async (req, res) => {
+	console.log("### organisedActivityId:", req.query.organisedActivityId);
+	const id = req.query.organisedActivityId;
+
+	try {
+		const result = await OrganisedActivity.findOne({
+			where: {
+				id,
+			},
+			include: [
+				{
+					model: OrganisedActivityAttendence,
+					attributes: ["id"],
+					include: [
+						{
+							model: Resident,
+							attributes: ["id", "firstName", "lastName"],
+						},
+					],
+				},
+				{
+					model: Activity,
+					attributes: ["activity"],
+				},
+			],
+		});
+
+		console.log("### result", result);
+		res.json(result);
+	} catch (error) {
+		logger.error("GET activity/organisedActivity", error);
+		res.send("ERROR: activity/organisedActivity");
 	}
 });
 
