edc3631f26ff2a639446fec662e928c6227eb892
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2024-01-25 12:10:51 +1100

    setup resident/pieChartData route

--- routes/residentRoutes.js
+++ routes/residentRoutes.js
@@ -182,4 +182,48 @@ router.get("/residentActivities", async (req, res) => {
 	}
 });
 
+router.get("/pieChartData", async (req, res) => {
+	try {
+		const randomGenerator = (number) => {
+			return Math.floor(Math.random() * number + 1);
+		};
+		const data = [
+			{
+				id: "bingo",
+				label: "bingo",
+				value: randomGenerator(8),
+				color: "hsl(104, 70%, 50%)",
+			},
+			{
+				id: "painting",
+				label: "painting",
+				value: randomGenerator(8),
+				color: "hsl(162, 70%, 50%)",
+			},
+			{
+				id: "gardening",
+				label: "gardening",
+				value: randomGenerator(8),
+				color: "hsl(291, 70%, 50%)",
+			},
+			{
+				id: "bustrip",
+				label: "bustrip",
+				value: randomGenerator(8),
+				color: "hsl(229, 70%, 50%)",
+			},
+			{
+				id: "music",
+				label: "music",
+				value: randomGenerator(8),
+				color: "hsl(344, 70%, 50%)",
+			},
+		];
+		res.json(data);
+	} catch (error) {
+		logger.error("GET resident/residentCharts:", error);
+		res.send("ERROR: resident/residentCharts");
+	}
+});
+
 module.exports = router;
